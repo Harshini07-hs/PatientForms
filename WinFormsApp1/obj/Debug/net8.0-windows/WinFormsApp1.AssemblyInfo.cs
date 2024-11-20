e5405a612843160c2bb906180bfe767fbd041561
jagadeesh <jagadeesh.m1@tataelxsi.co.in>
2024-11-20 14:16:19 +0530

    Updated DataFiles

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f738491f08d52bc9de166e72bd5fe1322b99c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1252e275e13ac1501abc7d158cd3dfbc41d49b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
